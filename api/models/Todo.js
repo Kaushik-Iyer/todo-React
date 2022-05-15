32de5b056c7fad78e18823e4ca5e526171608ca8
Kaushik-Iyer <kushlaser@gmail.com>
2022-05-15 22:58:52 +0530

    first commit

---
+++ api/models/Todo.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false,
+        sparse:true
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
+    }
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+
+
+export default Todo
